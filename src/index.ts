d9ec19fb0484c343ea40b340ac67d709d17c2173
Clement Lakhal <clementlakhal@gmail.com>
2019-10-05 02:41:55 +0200

    Update

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log('Hello Plop');
+import express from 'express';
+
+const app: express.Application = express();
+
+const port: number = 3000;
+
+app.get('/', (_: express.Request, res: express.Response) => {
+  res.send('Hello world!');
+});
+
+app.listen(port);
