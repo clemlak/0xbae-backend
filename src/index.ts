d417d3ea92c600de2c7b7e350b56f27e4a5411a5
Clement Lakhal <clementlakhal@gmail.com>
2019-10-05 03:15:06 +0200

    Update stuff

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import express from 'express';
+import cors from 'cors';
 
 const app: express.Application = express();
+const port: number = 3001;
 
-const port: number = 3000;
+app.use(cors());
 
 const posts = [
   {
