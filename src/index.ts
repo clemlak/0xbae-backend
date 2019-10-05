557831c832f2003f6fd181904a7c6f5bd4e388e6
Clement Lakhal <clementlakhal@gmail.com>
2019-10-05 02:49:24 +0200

    Update dummy data

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,31 @@ const app: express.Application = express();
 
 const port: number = 3000;
 
+const posts = [
+  {
+    src: 'https://preview.redd.it/9inzl7fl03q31.jpg?width=864&auto=webp&s=36a7d23bbc53dfde03f16a93d8f2213cf95bcd08',
+    model: 'Nedji',
+    address: '0x1',
+  },
+  {
+    src: 'https://preview.redd.it/0t6fso9u89q31.jpg?width=576&auto=webp&s=05b386bfa69d8f94bb550e5882b19f03515ac5be',
+    model: 'Natsu',
+    address: '0x2',
+  },
+  {
+    src: 'https://preview.redd.it/n29sz59215q31.jpg?width=576&auto=webp&s=0d1a8d6135f7a70419438263d2ceffa181d8ef19',
+    model: 'Loona',
+    address: '0x3',
+  },
+  {
+    src: 'https://i.redd.it/ysii047mf8q31.jpg',
+    model: 'Tokyo',
+    address: '0x4',
+  },
+];
+
 app.get('/', (_: express.Request, res: express.Response) => {
-  res.send('Hello world!');
+  res.json(posts);
 });
 
 app.listen(port);
